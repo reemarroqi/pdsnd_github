5adc661dfadb07f9cb734f6c928b00b20a46b5ba
reemarroqi <reemmajed99@gmail.com>
2023-09-13 21:53:01 +0300

    changed the way of setting an attribute of df to be cmpatible with newer python versions

--- bikeshare.py
+++ bikeshare.py
@@ -57,7 +57,7 @@ def load_data(city, month, day):
         df - Pandas DataFrame containing city data filtered by month and day
     """
     df = pd.read_csv(CITY_DATA[city])
-    df.name = city
+    df.attrs['name'] = city
     df['Start Time'] = pd.to_datetime(df['Start Time'])
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.day_name()
